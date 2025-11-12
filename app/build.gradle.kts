f76fef5c23cfcb2667391685dca52b9560f13f70
Tom <leduytam98ktmt@gmail.com>
2025-11-12 17:21:30 +0700

    setup compose first

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -46,4 +49,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
 }
